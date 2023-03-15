f8538914d6da2215aee755604fd94ffc68b384ee
empitsu <oneok0813@gmail.com>
2023-03-15 22:25:18 +0900

    fix

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const { PythonShell } = require('python-shell');
+const fs = require("fs");
+const { PythonShell } = require("python-shell");
 
 // URLを指定してPythonスクリプトを実行
-PythonShell.run('scrape_article.py', { args: ['https://example.com/article'] }, function (err, result) {
-  if (err) throw err;
-  const article = JSON.parse(result[0]);
-  console.log(article);
+// PythonShell.run(
+//   "scrape_article.py",
+//   { args: ["https://zenn.dev/revenuehack/articles/114d08d5082891"] },
+//   function (err, result) {
+//     if (err) throw err;
+//     const article = JSON.parse(result[0]);
+//     console.log(article);
+//   }
+// );
+PythonShell.run("scrape_article.py", {
+  args: ["https://zenn.dev/revenuehack/articles/114d08d5082891"],
+}).then((results) => {
+  const article = JSON.parse(results[0]);
+  // console.log(article);
+
+  fs.writeFileSync(`${article.title}.txt`, article.text);
 });
--- scrape_article.py
+++ scrape_article.py
@@ -1,12 +1,14 @@
 from newspaper import Article
 import json
 import sys
+import logging
 
 # URLを受け取り、記事のタイトルと本文を返す関数
 def scrape_article(url):
     article = Article(url, language='ja') # 言語が日本語の場合は language='ja' を指定する
     article.download()
     article.parse()
+    logging.info(article.text)
     return {'title': article.title, 'text': article.text}
 
 # Node.jsから受け取った引数を処理する
