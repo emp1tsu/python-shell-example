85294cf4411347ad58e5373e516aa88aa5df0148
empitsu <oneok0813@gmail.com>
2023-03-15 22:25:44 +0900

    fix

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 const fs = require("fs");
 const { PythonShell } = require("python-shell");
 
-// URLを指定してPythonスクリプトを実行
-// PythonShell.run(
-//   "scrape_article.py",
-//   { args: ["https://zenn.dev/revenuehack/articles/114d08d5082891"] },
-//   function (err, result) {
-//     if (err) throw err;
-//     const article = JSON.parse(result[0]);
-//     console.log(article);
-//   }
-// );
 PythonShell.run("scrape_article.py", {
   args: ["https://zenn.dev/revenuehack/articles/114d08d5082891"],
 }).then((results) => {
